7a54506949127322ae88eb7ea321286c27c3460a
Abdulloh <abdulloh50007@gmail.com>
2025-07-25 06:53:11 +0000

    V 1.7.1 release

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <AppProvider>
+          <AppProvider> 
             <div className="relative flex min-h-screen w-full flex-col">
               <Header />
               <main className="flex-1 pt-4 pb-20 md:pb-0">
