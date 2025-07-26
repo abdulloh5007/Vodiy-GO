5332c69b80512b3cc7e9652605bf3554b85d9611
Abdulloh <abdulloh50007@gmail.com>
2025-07-26 09:43:07 +0000

    добавь паузу на 2 секунд то есть после полной анимации текст должен оста

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -87,13 +87,15 @@ export default {
         },
         marquee: {
           '0%': { transform: 'translateX(0%)' },
-          '100%': { transform: 'translateX(-100%)' },
+          '20%': { transform: 'translateX(0%)' }, /* Start pause */
+          '80%': { transform: 'translateX(-100%)' },
+          '100%': { transform: 'translateX(-100%)' }, /* End pause */
         },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        'marquee': 'marquee 8s linear infinite',
+        'marquee': 'marquee 10s linear infinite',
       },
     },
   },
