e6792ea765cdf955159c5529e6a4c48539a8bddd
Abdulloh <abdulloh50007@gmail.com>
2025-07-29 12:30:51 +0000

    V 1.9.4

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -183,10 +183,10 @@ export function Header() {
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-16 items-center">
-        <Link href="/" className="mr-6 flex items-center space-x-2">
+        <div className="mr-6 flex items-center space-x-2"> 
           <RoadPilotLogo className="h-6 w-6 text-primary" />
           <span className="font-bold font-headline sm:inline-block">Vodiy GO</span>
-        </Link>
+        </div>
         
         <div className="flex flex-1 items-center justify-end space-x-2">
           <div className='flex items-center space-x-2'>
